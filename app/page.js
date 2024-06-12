80e01faa32890783265b221b78498863a28b5a72
Ayushatre05 <ayushkatre1801@gmail.com>
2024-06-12 10:37:52 +0530

    authentication done

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
-    <div><button class="btn btn-secondary">Secondary</button></div>
+    <div>
+      <UserButton/>
+    </div>
   )
 }
 
